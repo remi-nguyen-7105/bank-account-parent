8a2c7e9e7fd655ed33b4d2bccadcb07061f923bc
Nguyen Rémi <r.nguyen-ext@lefebvre-dalloz.fr>
2024-12-16 23:24:49 +0100

    changing service interface to hide Deposit & Withdrawal

--- bank-account/src/main/java/kata/bank/account/service/AccountService.java
+++ bank-account/src/main/java/kata/bank/account/service/AccountService.java
@@ -1,15 +1,14 @@
 package kata.bank.account.service;
 
-import kata.bank.account.domain.Deposit;
-import kata.bank.account.domain.Withdrawal;
+import java.math.BigDecimal;
 
 
 public interface AccountService {
     // US 1
-    void saveMoney(String clientId, Deposit deposit);
+    void saveMoney(String clientId, BigDecimal deposit);
 
     // US 2
-    void retrieveSavings(String clientId, Withdrawal withdrawal);
+    void retrieveSavings(String clientId, BigDecimal withdrawal);
 
     // requirement
     String printAccountStatement(String clientId);
